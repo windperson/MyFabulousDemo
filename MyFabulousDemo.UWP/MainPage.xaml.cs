f5dd68e2ea51daef1f07ee6eeb0d36aa1bdf3d34
Chen Yu Pao <windperson@gmail.com>
2019-03-06 02:31:56 +0800

    add origin template generated files

---
+++ MyFabulousDemo.UWP/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.Foundation;
+using Windows.UI.ViewManagement;
+
+namespace MyFabulousDemo.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            LoadApplication(new MyFabulousDemo.App());
+        }
+    }
+}
